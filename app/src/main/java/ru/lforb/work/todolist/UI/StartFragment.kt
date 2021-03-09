1027e1555095e9eb8220cce097cad013f6697b8a
Andrey Bachilo <andrew.bachilo@gmail.com>
2021-03-09 22:27:24 +0300

    refactoring2

--- app/src/main/java/ru/lforb/work/todolist/UI/StartFragment.kt
+++ app/src/main/java/ru/lforb/work/todolist/UI/StartFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
-import ru.lforb.work.todolist.R
 import ru.lforb.work.todolist.databinding.FragmentStartBinding
 
 
